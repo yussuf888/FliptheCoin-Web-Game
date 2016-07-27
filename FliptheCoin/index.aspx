80aa8d298f85e7c2a790977242cf072f6694791c
yussuf888 <yussuf888@gmail.com>
2016-07-27 09:47:13 +0800

    Add files via upload

---
+++ FliptheCoin/index.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="index.aspx.vb" Inherits="FliptheCoin.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head runat="server">
+
+        <meta name="msvalidate.01" content="E34A1A0526B70B2582F63C709A5095A7" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Just Try Your Luck By Flip The Coin</title>
+
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <link rel="stylesheet" type="text/css" href="css/font.css"/>  
+    
+    </head>
+
+    <body>
+
+        <h1>Tails or Heads Game</h1>
+        <p>Please select head or tail.</p>
+        <div class="container">
+	        <table>
+	 	        <tr>
+	  		        <td>
+	  			        <ul>
+	  				        <li>
+	    				        <input type="radio" id="option-tail" onclick="mySelect()" name="selector" value="Tail"/>
+	    				        <label for="option-tail">Tail</label>
+	    				        <div class="check"></div>
+					        </li>
+				        </ul>
+			        </td>
+			        <td>
+				        <ul>
+	  				        <li>
+	    				        <input type="radio" id="option-head" onclick="mySelect()" name="selector" value="Head"/>
+	    				        <label for="option-head">Head</label>
+	    				        <div class="check"></div>
+					        </li>
+				        </ul>
+			        </td>
+		        </tr>
+	        </table>
+        </div>
+        <input type="button" name="click" class="button" id="click" value="LET'S PLAY" />
+        <%--Online--<div id = "Coin"><img id = "myImage" src = "http://i.giphy.com/3oEjI73Es1Tblwy6T6.gif"/></div>--Online--%>
+        <div id = "Coin"><img id = "myImage" src = "image/turn-coin.gif"/></div>
+        <div id = "Box"><span id="result"></span></div>
+
+    </body>
+
+    <script type="text/javascript" src="script/jquery.js"></script>
+    <script type="text/javascript" src="script/flipcoin.js"></script>
+        
+</html>
---
+++ FliptheCoin/script/flipcoin.js
@@ -0,0 +1,95 @@
+﻿document.getElementById('click').onclick = click;
+
+function click()
+{
+    if (document.getElementById('option-tail').checked == true || document.getElementById('option-head').checked == true)
+    {
+        x = (Math.floor(Math.random() * 2) == 0);
+        if (x)
+        {
+            flip("Head");
+        }
+        else
+        {
+            flip("Tail");
+        }
+        (
+            function ()
+            {
+                var Coin  = document.getElementById("Coin");
+                var Box   = document.getElementById("Box");
+                var click = document.getElementById("click");
+                var show  = function ()
+                {
+                    Coin.style.display  = "block";
+                    Box.style.display   = "none";
+                    click.style.display = "none";
+                    setTimeout(hide, 3000);
+                }
+                var hide = function ()
+                {
+                    Coin.style.display  = "none";
+                    Box.style.display   = "block";
+                    click.value = "PLAY AGAIN";
+                    click.style.margin = "auto";
+                    click.style.display = "block";
+                    $("#option-head").prop("checked", false);
+                    $("#option-tail").prop("checked", false);
+                }
+                show();
+            }
+        )()
+    }
+    else
+    {
+        alert("Please select head or tail.");
+    }
+};
+
+function flip(Coin)
+{
+    if (Coin == "Head")
+    {
+        if (document.querySelector('input[name="selector"]:checked').value == "Head")
+        {
+            document.getElementById("result").innerHTML = '<img width="220" height="220" src=\'image/head.png\'><p class="result">Head has been chosen. You won!!!</p>';
+        }
+        else
+        {
+            document.getElementById("result").innerHTML = '<img width="220" height="220" src=\'image/head.png\'><p class="result">Head has been chosen. Sorry you lose!!! </p>';
+        }
+    }
+    else
+    {
+        if (document.querySelector('input[name="selector"]:checked').value == "Tail")
+        {
+            document.getElementById("result").innerHTML = '<img width="220" height="220" src=\'image/tail.png\'><p class="result">Tail has been chosen. You won!!!</p>';
+        }
+        else
+        {
+            document.getElementById("result").innerHTML = '<img width="220" height="220" src=\'image/tail.png\'><p class="result">Tail has been chosen. Sorry you lose!!! </p>';
+        }
+    }
+};
+
+function mySelect()
+{
+    document.getElementById("result").innerHTML = '<img style="margin-top: 0px; margin-bottom: 30px;" width="220" height="220" src=\'image/middle.png\'>';
+}
+
+$("#click").click(function ()
+{
+    $('html, body').animate(
+    {
+        scrollTop: $("#Coin").offset().top
+    }, 2000);
+});
+
+/*
+var data = '&r=' + escape(document.referrer) + '&n=' + escape(navigator.userAgent) + '&p=' + escape(navigator.userAgent) + '&g=' + escape(document.location.href);
+if (navigator.userAgent.substring(0, 1) > '3')
+data = data + '&sd=' + screen.colorDepth + '&sw=' + escape(screen.width + 'x' + screen.height);
+document.write('<a href="http://www.1freecounter.com/stats.php?i=117922" target=\"_blank\" >');
+document.write('<img alt="Free Counter" style="display: none;" border=0 hspace=0 ' + 'vspace=0 src="http://www.1freecounter.com/counter.php?i=117922' + data + '">');
+document.write('</a>');
+*/
